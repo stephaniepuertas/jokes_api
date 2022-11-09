034a5597607dae1c1239179d870f8d2a9b6fb24f
stephaniepuertas <puertasstephanie@gmail.com>
2022-11-08 16:40:02 -0800

    added validations

--- models/jokes.model.js
+++ models/jokes.model.js
@@ -6,17 +6,23 @@ const JokeSchema = new mongoose.Schema({
     //defining what database should look like:
     //attributes of things within schema
     setup:{                      // set object
-        type:String             // object type
+        type:String,             // object type
+        required:[true, "Setup is required"], //for validations
+        minlength:[10, "Setup must be at least 10 characters long"]//validation
+
     },
     punchline:{
-        type:String
-    },
-    age: {
-        type:Number
-    },
-    tropical: {
-        type: Boolean
+        type:String,
+        required:[true, "Punchline is required"], //for validations
+        minlength:[3, "Punchline must be at least 3 characters long"]
     }
+    // ,
+    // age: {
+    //     type:Number
+    // },
+    // tropical: {
+    //     type: Boolean
+    // }
 }, {timestamps:true}); //if you dont put this your created at and updated at wont exist in schema
 
 //register schema into the actual models of database
